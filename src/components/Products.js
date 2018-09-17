0c4fed8929fb3fdc2759fd4e4dcb70b5bfb25251
Philip Boardman <dustykeyboard@brd.mn>
2018-09-17 22:04:10 +1000

    Pagination

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,6 +8,24 @@ class Products extends PureComponent {
       page: 1,
       perpage: 8,
       products: [],
+      pages: 1,
+    }
+
+    this.handlePageChange = this.handlePageChange.bind(this)
+    this.handlePerPageChange = this.handlePerPageChange.bind(this)
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const begin = Math.max(
+      parseInt(state.perpage, 10) * (parseInt(state.page, 10) - 1),
+      0,
+    )
+    const end = begin + parseInt(state.perpage, 10)
+    console.log('filter', begin, end)
+
+    return {
+      products: props.products.items.slice(begin, end),
+      pages: Math.ceil(props.products.items.length / state.perpage),
     }
   }
 
@@ -16,23 +34,46 @@ class Products extends PureComponent {
     if (!loaded && !loading) fetchProducts()
   }
 
-  static getDerivedStateFromProps(props, state) {
-    const start = Math.max(0, state.perpage * (state.page - 1))
-    const end = start + state.perpage
+  handlePageChange(event) {
+    this.setState({ page: event.target.value })
+  }
 
-    return { products: props.products.items.slice(start, end) }
+  handlePerPageChange(event) {
+    this.setState({ perpage: event.target.value })
   }
 
   render() {
     const { products: { loading, error } } = this.props
-    const { products, page, perpage } = this.state
+    const { products, pages, page, perpage } = this.state
+    const pageOptions = []
+    for (var i = 1; i <= pages; i++) {
+      pageOptions.push(i)
+    }
 
     return (
       <div>
         <h2>Products</h2>
         <p>This is the products page</p>
+
         <p>
-          Page {page}, {perpage} per page
+          Page
+          <select onChange={this.handlePageChange} value={page}>
+            {pageOptions.map(o => (
+              <option key={o} value={o}>
+                {o}
+              </option>
+            ))}
+          </select>
+        </p>
+
+        <p>
+          Per Page
+          <select onChange={this.handlePerPageChange} value={perpage}>
+            <option value={8}>8</option>
+            <option value={20}>20</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </select>
         </p>
 
         {error && <p className="error">{error.message}</p>}
@@ -43,6 +84,7 @@ class Products extends PureComponent {
           <ul className="products">
             {products.map(product => (
               <li key={product.id} className="product">
+                {product.id}.
                 {product.product_name}
               </li>
             ))}
