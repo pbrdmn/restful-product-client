b5460648c282aabea7fe08f994a354fa988529ea
Philip Boardman <dustykeyboard@brd.mn>
2018-09-17 21:48:25 +1000

    Render {perpage} products

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,19 +3,51 @@ import React, { PureComponent } from 'react'
 class Products extends PureComponent {
   constructor(props) {
     super(props)
+
+    this.state = {
+      page: 1,
+      perpage: 8,
+      products: [],
+    }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     const { loaded, loading, fetchProducts } = this.props
     if (!loaded && !loading) fetchProducts()
   }
 
+  static getDerivedStateFromProps(props, state) {
+    const start = Math.max(0, state.perpage * (state.page - 1))
+    const end = start + state.perpage
+
+    return { products: props.products.items.slice(start, end) }
+  }
+
   render() {
-    console.log(this.props)
+    const { products: { loading, error } } = this.props
+    const { products, page, perpage } = this.state
+
     return (
       <div>
         <h2>Products</h2>
         <p>This is the products page</p>
+        <p>
+          Page {page}, {perpage} per page
+        </p>
+
+        {error && <p className="error">{error.message}</p>}
+
+        {loading && <div className="loading">Loading Products</div>}
+
+        {products.length > 0 && (
+          <ul className="products">
+            {products.map(product => (
+              <li key={product.id} className="product">
+                {product.product_name}
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     )
   }
--- src/services/products.js
+++ src/services/products.js
@@ -1,7 +1,6 @@
 const api = 'https://whitechdevs.github.io/reactjs-test/products.json'
 
 async function fetchProducts() {
-  console.log('fetching products')
   return fetch(api).then(response => response.json())
 }
 
